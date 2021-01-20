f6e2bd99a80bf92063b7ddf5805e0db1cba3fc8e
Kerem <kerem_bolt@hotmail.com>
2021-01-20 23:55:22 +0300

    20 Ocak Ders Sonu.

---
+++ Donguler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirici Kursu","programlama giriş","java","Python","C#"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+                
+            }
+
+            // aşağıdaki diziler için daha çok kullanılan döngüdür.
+
+            Console.WriteLine("for bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+            Console.WriteLine("Sayfa Sonu-footer");
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string Aciklama = "Amasya elması";
+
+
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Dıyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                
+
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------------");
+
+            }
+
+            Console.WriteLine("----------Metotlar-----------");
+
+            //instance-class örneği oluşturmak
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Dıyarbakır karpuzu", 12, 8);
+
+        }
+    }
+}
+
+
